07fe1cd64be8e824203942de8af478e25b72d261
R_HYTE <polycarpwathuta39@gmail.com>
2024-04-27 00:20:18 +0300

    fix: Implement handleProfileSignup using Promise.all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,13 +1,17 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
+function handleProfileSignup() {
+  Promise.allSettled([uploadPhoto(), createUser()])
     .then((results) => {
-      const { body } = results[0];
-      const { firstName, lastName } = results[1];
-      console.log(`${body} ${firstName} ${lastName}`);
-    })
-    .catch(() => {
-      console.log('Signup system offline');
+      const errors = results.filter((result) => result.status === 'rejected');
+      if (errors.length > 0) {
+        console.log('Signup system offline');
+      } else {
+        const photo = results[0].value.body;
+        const user = results[1].value;
+        console.log(`${photo} ${user.firstName} ${user.lastName}`);
+      }
     });
 }
+
+export default handleProfileSignup;
