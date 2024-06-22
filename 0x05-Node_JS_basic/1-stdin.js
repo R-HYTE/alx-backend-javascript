faa4f198d83fd475b637792c49afd3e6f6209313
R_HYTE <polycarpwathuta39@gmail.com>
2024-06-22 20:38:17 +0300

    fix: simplify logic used

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,18 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+const readline = require('readline');
 
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}\n`);
-  process.stdin.end();
+const userInput = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
 });
 
-process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
+console.log('Welcome to Holberton School, what is your name?');
+
+userInput.on('line', (name) => {
+  console.log(`Your name is: ${name}`);
+  userInput.close();
+});
+
+userInput.on('close', () => {
+  console.log('This important software is now closing');
+  process.exit(0);
 });
