d2ac17b451fbe666120c5f182294f677cfb3e558
R_HYTE <polycarpwathuta39@gmail.com>
2024-06-23 01:09:37 +0300

    Add HTTP server created using the http module

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const PORT = 1245;
+
+const app = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running and listening on port ${PORT}`);
+});
+
+module.exports = app;
