662cf759a30cebcb09fed85547260792a0424936
R_HYTE <polycarpwathuta39@gmail.com>
2024-06-27 13:20:17 +0300

    refactor: simplify the complex HTTP server using Express

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,45 +1,86 @@
 const express = require('express');
-const { PassThrough } = require('stream');
-const { StringDecoder } = require('string_decoder');
-const countStudents = require('./3-read_file_async');
+const fs = require('fs');
 
 const app = express();
-const PORT = 1245;
-const databasePath = process.argv[2];
 
-app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
-});
+// Read CSV file asynchronously
+function readCSVFile(databaseFile, callback) {
+  fs.readFile(databaseFile, 'utf8', (err, data) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        callback(new Error('Cannot load the database'), null);
+      } else {
+        callback(err, null);
+      }
+      return;
+    }
 
-app.get('/students', async (req, res) => {
-  try {
-    const outputStream = new PassThrough();
-    const decoder = new StringDecoder('utf8');
-    let outputData = '';
+    // Splitting CSV into lines and filtering out empty lines
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
 
-    outputStream.on('data', (chunk) => {
-      outputData += decoder.write(chunk);
-    });
-
-    outputStream.on('end', () => {
-      outputData += decoder.end();
-      res.send(`This is the list of our students\n${outputData}`);
-    });
-
-    const originalConsoleLog = console.log;
-    console.log = (...args) => {
-      outputStream.write(`${args.join(' ')}\n`);
+    const students = {
+      CS: [],
+      SWE: [],
     };
 
-    await countStudents(databasePath);
+    // Skip the first line assuming it's the header
+    for (let i = 1; i < lines.length; i += 1) {
+      const line = lines[i];
+      const parts = line.split(',');
 
-    outputStream.end();
-    console.log = originalConsoleLog;
-  } catch (error) {
-    res.status(500).send('Cannot load the database');
-  }
+      if (parts.length === 4) {
+        const firstname = parts[0].trim();
+        const field = parts[3].trim();
+
+        if (field === 'CS') {
+          students.CS.push(firstname);
+        } else if (field === 'SWE') {
+          students.SWE.push(firstname);
+        }
+      }
+    }
+
+    callback(null, students);
+  });
+}
+
+app.get('/', (req, res) => {
+  res.type('text/plain').send('Hello Holberton School!');
 });
 
+app.get('/students', (req, res) => {
+  const databaseFile = process.argv[2];
+
+  if (!databaseFile) {
+    res.status(500).send('Internal Server Error: Missing database file argument');
+    return;
+  }
+
+  readCSVFile(databaseFile, (err, students) => {
+    if (err) {
+      res.status(500).send('This is the list of our students\nCannot load the database');
+      return;
+    }
+
+    let response = 'This is the list of our students\n';
+
+    // Total number of students
+    const totalStudents = students.CS.length + students.SWE.length;
+    response += `Number of students: ${totalStudents}\n`;
+
+    // Listing students in CS
+    const csCount = students.CS.length;
+    response += `Number of students in CS: ${csCount}. List: ${students.CS.join(', ')}\n`;
+
+    // Listing students in SWE
+    const sweCount = students.SWE.length;
+    response += `Number of students in SWE: ${sweCount}. List: ${students.SWE.join(', ')}`;
+
+    res.type('text/plain').send(response);
+  });
+});
+
+const PORT = 1245;
 app.listen(PORT, () => {
   console.log(`Server is running and listening on port ${PORT}`);
 });
