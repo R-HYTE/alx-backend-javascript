5da8b70894192df72e7f2f1215eef96bb68f9c1b
Polycarp <polycarpwathuta39@gmail.com>
2024-07-26 16:36:27 +0300

    feat: Add Teacher interface

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,27 @@
+interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [key: string]: any;
+}
+
+// Create a Teacher object
+const teacher3: Teacher = {
+    firstName: 'John',
+    lastName: 'Doe',
+    fullTimeEmployee: false,
+    location: 'London',
+    contract: false,
+};
+
+console.log(teacher3);
+
+// In console should print
+// Object
+// contract: false
+// firstName: "John"
+// fullTimeEmployee: false
+// lastName: "Doe"
+// location: "London"
